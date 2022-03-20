c3b92a11cf02258dc75e3ecd1c1e3429df6e72ee
sushant dhimal <sushant07.dhimal@gmail.com>
2022-03-20 17:57:40 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+        <p className="text-3xl font-bold ">Hello developer.
+        Now you can use <span className="text-green-300">Tailwind CSS</span>
+        </p>
       </header>
     </div>
   );
