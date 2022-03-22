e9383e8a922ebbe3815d4675462f80a81a48af2b
sushant dhimal <sushant07.dhimal@gmail.com>
2022-03-22 19:36:54 +0545

    adding CardListfile

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 const card = (props) => {
-  const { id, name, email } = props;
   return (
-    <div className=" bg-lime-400 inline-block hover:scale-105 m-2  text-center shadow-lime-500 shadow-lg rounded-md">
-      <img src={`https://robohash.org/${id}?size=200x200`} alt="robots"></img>
+    <div className="bg-lime-400 inline-block hover:scale-105 m-2  text-center shadow-lime-500 shadow-lg rounded-md">
+      <img
+        src={`https://robohash.org/${props.id}?size=200x200`}
+        alt="robots"
+      ></img>
       <div>
-        <strong>{name}</strong>
-        <p>{email}</p>
+        <strong>{props.name}</strong>
+        <p>{props.email}</p>
       </div>
     </div>
   );
