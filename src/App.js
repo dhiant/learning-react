2682c4e8d14804d042219dad7bc76087497e2a0c
Sushant Dhimal <sushant07.dhimal@gmail.com>
2022-03-30 22:33:29 +0545

    adding lifecycle method - componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React from "react";
 import CardList from "./CardList";
-import { robots } from "./robots_name";
 import SearchBox from "./SearchBox";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
   }
   onSearchChange = (event) => {
     this.setState({
-      searchfield: event.target.value
+      searchfield: event.target.value,
     });
   };
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          robots: json,
+        });
+      });
+  }
   render() {
     const filterRobot = this.state.robots.filter((robot) => {
       return robot.name
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="text-center ">
-        <h2 className="font-bold text-4xl text-emerald-200 robotstitle">COOL ROBOTS</h2>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filterRobot} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1 className="text-white">Loading....</h1>;
+    } else {
+      return (
+        <div className="text-center ">
+          <h2 className="font-bold text-4xl text-emerald-200 robotstitle">
+            COOL ROBOTS
+          </h2>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filterRobot} />
+        </div>
+      );
+    }
   }
 }
 
